d04d432d8d7875243b5c89250ab3315ab36292bb
Jared DeMarais <demaraj198@potsdam.edu>
2019-02-22 10:30:56 -0500

    Update settings.py

--- DockerFiles/autograder/autograder/settings.py
+++ DockerFiles/autograder/autograder/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'autograder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodocker_db',
+        'USER': 'django',
+        'PASSWORD': 'django',
+        'HOST': 'db',
+        'PORT': '3306',
     }
 }
 
