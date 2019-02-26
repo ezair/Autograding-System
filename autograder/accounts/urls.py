dd1b30b6f5d815f361c5146a1728a2c7695621b9
EZ <zaireric@gmail.com>
2019-02-26 00:42:45 -0500

    Added urlpatterns list (currently just a stem)

---
+++ autograder/accounts/urls.py
@@ -0,0 +1,17 @@
+'''
+Created by:	Eric Zair
+File: accounts/urls.py
+Description:	Contains and manages the paths to all of our pages in
+				the accounts/ application.
+				This will include things like logins, logouts, student registrations,
+				...etc
+Last edited by:	Eric Zair
+Last edited on:	02/25/2019
+'''
+from django.urls import path, include
+
+
+# All accounts application paths and configurations.
+urlpatterns = [
+	# Currently a stem.
+]
