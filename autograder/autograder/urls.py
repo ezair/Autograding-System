3986bb3ac00aa0f888809a880ea3c842a44d0e4f
EZ <zaireric@gmail.com>
2019-02-26 00:44:14 -0500

    added staticfiles_urlpatterns(), so that static files can be referenced from any application location

--- autograder/autograder/urls.py
+++ autograder/autograder/urls.py
@@ -14,8 +14,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+'''
+Description:
+Last edited by: Eric Zair
+Last edited on: 02/26/2019
+'''
 
+
+# Simply contains the paths to different applications in our project.
+# This list of urls will ONLY have paths to default apps and to our homepage.
+# NOTE:	Path to homepage should be '/'.
+#	EVEN MORE IMPORTANT NOTE:
+#		When it comes to using the "name=" parameter, always name the link
+#		name='application_name-the_name_of_the_template'
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls'), name='accounts'),
 ]
+
+# So that we can load static files properly from ALL url locations.
+# This DOES NOT need to be added to any other urls.py files.
+urlpatterns += staticfiles_urlpatterns()
