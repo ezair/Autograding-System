a4fc0c3ba3b88bfa8256e285362b925f6d91fdf5
EZ <zaireric@gmail.com>
2019-02-27 20:42:23 -0500

    Added more comments to urls.py

--- autograder/accounts/urls.py
+++ autograder/accounts/urls.py
@@ -6,14 +6,15 @@ Description:	Contains and manages the paths to all of our pages in
 				This will include things like logins, logouts, student registrations,
 				...etc
 Last edited by:	Eric Zair
-Last edited on:	02/26/2019
+Last edited on:	02/27/2019
 '''
 from django.urls import path, include
 from .views import *
 
 
-# All accounts application paths and configurations.
+# All /accounts application pages here.
+# Make sure to add the name = param, or everything will just suck.
 urlpatterns = [
 	path('register/', StudentRegistrationView.as_view(),
-				   name='accounts-student_registration'),
-]
+				  	name='accounts-student_registration'),
+	]
