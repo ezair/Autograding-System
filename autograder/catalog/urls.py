a9ff8e40f78779d87a0be19562c90192311a51e0
EZ <zaireric@gmail.com>
2019-03-01 21:27:39 -0500

    I changed the path to the index.html file located in catalog/templates/catalog/index.html. Now the view points to catalog/index.html this is standard django style.

--- autograder/catalog/urls.py
+++ autograder/catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+# Stick to the nameing conventions for the name= parameter.
+# it is always 'app_name-html_file_name' and nothing else.
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='catalog_index'),
 ]
