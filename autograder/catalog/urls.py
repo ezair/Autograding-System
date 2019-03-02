c0bc8121e3203a072790ddfad0ce93b357e478ef
EZ <zaireric@gmail.com>
2019-03-01 21:40:21 -0500

    changed name parameter in path for catalog/ homepage

--- autograder/catalog/urls.py
+++ autograder/catalog/urls.py
@@ -4,5 +4,5 @@ from . import views
 # Stick to the nameing conventions for the name= parameter.
 # it is always 'app_name-html_file_name' and nothing else.
 urlpatterns = [
-    path('', views.index, name='catalog_index'),
+    path(''	, views.index, name='catalog-index'),
 ]
