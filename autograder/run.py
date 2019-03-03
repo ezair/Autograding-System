4cd0e94e9332e094a729d06b996a6a189a5b33fd
EZ <zaireric@gmail.com>
2019-03-03 18:27:17 -0500

    Refactored and removed wait method

--- autograder/run.py
+++ autograder/run.py
@@ -26,42 +26,8 @@ import os
 import sys
 from os import system
 
-
-'''
-	Purpose:	This method is here to ensure that our db service (postgres server)
-				runs before our web service (django server), because the web service
-				relies on the db service.
-
-	Why this is important:	If the web service were to run first, it would not be able
-							to connect to the db service, and as a result, it will throw
-							an exception and not allow us to connect to the postgres server.
-							NOTE:
-								This function makes sure this issue does NOT happen.
-'''
-def wait_for_db():
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	# Keep trying to connect to the postgres database, until
-	# we are finally able to connect.
-	# After that, we can just close our connection.
-	while True:
-		# Connect to the postgres server, so that we can see if it is
-		# currently running.
-		# 		IMPORTANT NOTE:
-		#			The port MUST match the one in "settings.py" file.
-		#			...basically, we should never change it, or it will be an issue.
-		try:
-			s.connect(('127.0.0.1', 5432))
-			s.close()
-			break
-		# Server is not yet running, so we re-loop and try again.
-		except socket.error as ex:
-			print("Attemping to connect to postgres server")
-			time.sleep(0.1)
-
 #_______________________________________________________________________
 def main():
-	# postgres server must be running before the django server,
-	wait_for_db()
 	
 	# Allows us to run a docker-compose down.
 	if 'down'.strip() in sys.argv:
