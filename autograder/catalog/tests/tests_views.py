c2edeeadb8e3ee48026caa1e542b48feb34ceb8f
Christopher Stannard <Cstannard@ymail.com>
2019-03-05 20:26:44 -0500

    Tests for the index view

--- autograder/catalog/tests/tests_views.py
+++ autograder/catalog/tests/tests_views.py
@@ -2,20 +2,43 @@ from django.test import TestCase
 from django.urls import reverse
 from catalog.views import *
 
-class TestIndexViews(TestCase):
+from django.contrib.auth.models import User
+from django.test import Client
+
+
+class IndexViewTest(TestCase):
     """Test for index view"""
     def test_index_template(self):
         response = self.client.get(reverse('catalog-index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'catalog/index.html')
 
+class LogInPassTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
     def test_login(self):
+        # send login data
+        response = self.client.post('/catalog/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertTrue(response.context['user'].is_active)
+
+class LogInFailTest(TestCase):
+    def setUp(self):
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user('username', email='None', password='None')
+    def test_login(self):
+        # send login data
+        response = self.client.post('/catalog/', self.credentials, follow=True)
+        # should be logged in now
+        self.assertFalse(response.context['user'].is_active)
+
+class RegistrationRedirectTest(TestCase):
+
+    def test_the_redirect(self):
         response = self.client.get(reverse('catalog-index'))
-        self.assertEqual(response.status_code, 200)
-        post_response = self.client.post(reverse('catalog-index'),
-            data={'exampleInputEmail1': "test@example.com",
-                  'password1': "icantthinkofanything"})
-        self.assertEqual(post_response.status_code, 302)
-        user = CustomUser.objects.get(id=1)
-        #user = User.objects.get(id=1)
-        self.assertEqual(user.email, 'test@example.com')
+        self.assertContains(response, '<a href="%s">here</a>' % reverse("accounts-student_registration"), html=True)
