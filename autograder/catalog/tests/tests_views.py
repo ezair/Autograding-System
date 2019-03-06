88f6c2aacd1749cd19e2a1a44b99ef4e5183b537
Christopher Stannard <Cstannard@ymail.com>
2019-03-06 10:41:31 -0500

    Added comments

--- autograder/catalog/tests/tests_views.py
+++ autograder/catalog/tests/tests_views.py
@@ -1,3 +1,10 @@
+'''
+Created by:	Chris Stannard
+Description: This is the test file for the catalog views
+Last edited by:	Chris Stannard
+Last edited on:	03/06/2019
+'''
+
 from django.test import TestCase
 from django.urls import reverse
 from catalog.views import *
@@ -6,18 +13,19 @@ from django.contrib.auth.models import User
 from django.test import Client
 
 
-class IndexViewTest(TestCase):
+class IndexTemplateTest(TestCase):
     """Test for index view"""
     def test_index_template(self):
         response = self.client.get(reverse('catalog-index'))
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'catalog/index.html')
 
-class LogInPassTest(TestCase):
+class IndexLogInPassTest(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'testuser',
             'password': 'secret'}
+        # create user using credentials
         User.objects.create_user(**self.credentials)
     def test_login(self):
         # send login data
@@ -25,20 +33,23 @@ class LogInPassTest(TestCase):
         # should be logged in now
         self.assertTrue(response.context['user'].is_active)
 
-class LogInFailTest(TestCase):
+class IndexLogInFailTest(TestCase):
     def setUp(self):
         self.credentials = {
             'username': 'testuser',
             'password': 'secret'}
-        User.objects.create_user('username', email='None', password='None')
+        # create user using different credentials
+        User.objects.create_user('usertest', password='secret')
     def test_login(self):
         # send login data
         response = self.client.post('/catalog/', self.credentials, follow=True)
-        # should be logged in now
+        # should not be logged in now
         self.assertFalse(response.context['user'].is_active)
+        # a message should be viewable
+        self.assertContains(response, 'Please enter a correct username and password. Note that both fields may be case-sensitive.')
 
-class RegistrationRedirectTest(TestCase):
-
+class IndexRedirectToRegistrationTest(TestCase):
     def test_the_redirect(self):
         response = self.client.get(reverse('catalog-index'))
+        # checks to see if the link is there and gets the correct template
         self.assertContains(response, '<a href="%s">here</a>' % reverse("accounts-student_registration"), html=True)
