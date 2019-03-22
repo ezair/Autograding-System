b344d2d026bdc4f547fd5cc06742631f01303adc
EZ <zaireric@gmail.com>
2019-03-21 22:32:33 -0400

    Added the my.html template and redirects

--- autograder/catalog/tests/tests_views.py
+++ autograder/catalog/tests/tests_views.py
@@ -1,24 +1,37 @@
 '''
 Created by:	Chris Stannard
 Description: This is the test file for the catalog views
-Last edited by:	Chris Stannard
-Last edited on:	03/06/2019
+Last edited by:	Eric Zair
+Last edited on:	03/21/2019
 '''
 
 from django.test import TestCase
 from django.urls import reverse
 from catalog.views import *
-
 from django.contrib.auth.models import User
 from django.test import Client
 
 
-class IndexTemplateTest(TestCase):
-    """Test for index view"""
-    def test_index_template(self):
-        response = self.client.get(reverse('catalog-index'))
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'catalog/index.html')
+class CatalogTemplateTests(TestCase):
+	"""Test for index view"""
+	def test_index_template(self):
+		response = self.client.get(reverse('catalog-index'))
+		self.assertEqual(response.status_code, 200)
+		self.assertTemplateUsed(response, 'catalog/index.html')
+	
+	# create user for testing reasons.
+	def setUp(self):
+		self.credentials = {'username': 'testuser', 'password': 'secret'}
+		User.objects.create_user('usertest', password='secret')
+'''
+	def test_my_template(self):
+		client = Client()
+		login = client.login(username='testuser', password='secret')
+		# self.assertTrue(login)
+		response = self.client.get(reverse('catalog-my'))
+		self.assertEqual(response.status_code, 302)
+		self.assertTemplateUsed(response, 'catalog/my.html')
+'''
 
 class IndexLogInPassTest(TestCase):
     def setUp(self):
--- autograder/catalog/views.py
+++ autograder/catalog/views.py
@@ -1,1 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.template import Context, loader
+from django.contrib.auth.decorators import login_required
+
+
+# This is the view for the location of a user's classes and thing of that sort.
+# If you are logged in, you get sent to the page.
+# Not logged in? Then you redirected to the login page. Upon logging, you are
+# then directed back to the page you were trying to
+@login_required
+def my_view(request):
+    	template = loader.get_template('catalog/my.html')
+    	return HttpResponse(template.render())
