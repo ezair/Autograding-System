68941070e7b1aa23ed58f442a443761de6e889a4
EZ <zaireric@gmail.com>
2019-04-17 16:08:50 -0400

    cleanup and refactored view for registraion. I did this so that registering a student in make_invite_view is not redundant

--- autograder/accounts/views.py
+++ autograder/accounts/views.py
@@ -56,7 +56,7 @@ def send_confimation_email(username, password):
 def send_join_course_email(invite_receivers, course, group,
 						   invite_sender='autograderinstructor@gmail.com'):
 	subject = "Welcome to " + course + "."
-	message = str(invite_sender) + " has invited you to " + str(course)
+	message = "You have been registered to join " + str(course)
 	message += " as a " + str(group) + "."
 	# Currently this works (sends an email to 1 student), but eventually
 	# it will work for multiple.
@@ -64,6 +64,29 @@ def send_join_course_email(invite_receivers, course, group,
 		invite_receivers[i] += "@potsdam.edu"
 	email = EmailMultiAlternatives(subject, message, invite_sender, invite_receivers)
 	email.send()
+
+
+# parses data given by a django form object.
+# We then create a user account based off of that form.
+def register_user_account(form):
+	username = form.cleaned_data['username']
+	email_address = str(username) + '@potsdam.edu'
+	password = User.objects.make_random_password()
+	# We NEED to make sure that we are NOT trying to create a duplicate user
+	# accounts. Because that just sucks. Like omg it really sucks. 
+	# It's actually not fun at all.
+	if not User.objects.filter(username=username).exists():
+		# Basically, we create the user account and fill in default information
+		# so that the admin does not know the password of the user, but we are
+		# still able to email all of the needed information to said user.
+		user = User.objects.create_user(username=username,
+										password=password,
+										email=email_address)
+		user.save()
+		# Handles generic layout of sending emails.
+		# Self made method (listed above this view).
+		send_confimation_email(username=username, password=password)
+
 
 #VIEWS BELOW_________________________________________________________________________________
 
@@ -75,31 +98,9 @@ def register_account_view(request):
 	# Grab the information from the user and make sure that the
 	# email field has been filled out successfully.
 	if request.POST and form.is_valid():
-		email_address = form.cleaned_data['email']
-		username = email_address.split('@')[0]
-		password = User.objects.make_random_password()
-
-		# We NEED to make sure that we are NOT trying to create a duplicate user
-		# accounts. Because that just sucks. Like omg it really sucks. 
-		# It's actually not fun at all.
-		if not User.objects.filter(username=username).exists():
-			# Basically, we create the user account and fill in default information
-			# so that the admin does not know the password of the user, but we are
-			# still able to email all of the needed information to said user.
-			user = User.objects.create_user(username=username,
-											password=password,
-											email=email_address)
-			user.save()
-
-			# Handles generic layout of sending emails.
-			# Self made method (listed above this view).
-			send_confimation_email(username=username, password=password)
-		
+		register_user_account(form)
 		return render(request, 'accounts/registration.html', {'form' : form})
-
-	# This will be changed shortly. Varying on what we choose to do.
-	# I will think about if we want to redirect, or have a "add another page".
-	# Again...this will come later, it doesn't matter right now.
+	# Not sure where we want to redirect quite yet.
 	return render(request, 'accounts/registration.html', {'form' : form})
 
 
@@ -114,30 +115,30 @@ def make_invite_view(request):
 		add_to_group = form.cleaned_data['group_choice']
 	
 		# We need to create the appropriate model for
-		# the approriate group.
+		# the approriate group given in the view dropdown menu.
 		if add_to_group == 'Student':
 			# We only want to add a student to the course, if the record does not exist.
 			if not models.Take.objects.filter(student=invite_receiver, course=course).exists():
 				models.Take.objects.create(student=invite_receiver, course=course).save()
-				send_join_course_email(invite_receivers=[str(invite_receiver)],
-									   course=str(course), group='Student')
-				form.save()
 
 		# THIS WILL ALSO ADD INSTRUCTOR TO GRADER ROLE BY DEFAULT....eventually...
 		elif add_to_group == 'Instructor':
 			# We only want to make a instructor, if the record does not already exist.
 			if not models.Instruct.objects.filter(instructor=invite_receiver, course=course).exists():
-				models.Instruct.objects.create(instructor=invite_receiver,
-											   course=course)
-				send_join_course_email(invite_receivers=[str(invite_receiver)],
-								       course=str(course), group='Instructor')
-				form.save()
-			## WE NEED TO ADD GRADER HERE
-			## WE NEED A GRADES MODEL.
+				models.Instruct.objects.create(instructor=invite_receiver, course=course).save()
 		
+		# Form is valid, so we store into our database, and then email the user.
+		form.save()
+		send_join_course_email(invite_receivers=[str(invite_receiver)],
+							   course=str(course), group=add_to_group)
+
 		# Make sure the user is actually registered to the proper group.
+		# Even if they are in the group already, we do this to be safe.
 		group = Group.objects.get(name=str(add_to_group))
 		group.user_set.add(invite_receiver)
 
 		return render(request, 'accounts/make_invite.html', {'form' : form})
+	
+	# Have not decided if we want to redirect the page, or just yield a
+	# pop up message. TBA.
 	return render(request, 'accounts/make_invite.html', {'form': form})
