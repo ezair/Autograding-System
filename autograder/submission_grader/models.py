9e7de8cb0c7a4982477250c8d1aaa08c42772a49
EZ <zaireric@gmail.com>
2019-04-28 00:18:17 -0400

    Submissions work. At least in the sense that you can submit a file (locally) to a specific path. The path being: assignment_pk/student_username/name_of_file

--- autograder/submission_grader/models.py
+++ autograder/submission_grader/models.py
@@ -8,10 +8,26 @@ Last Edited by:	Eric Zair
 '''
 from django.db import models
 
+
+# This will return the location that a submission will be stored in.
+#The format is:
+#				assignment_primary_key/student_username
+def get_files_path(submission, filename):
+	return str(submission.assignment.pk) + '/' + str(submission.student) + '/' + filename
+
+
+# When a student submits a project for a given assignment, we must store it in the
+# database of course...hence this Submission model.
 class Submission(models.Model):
 	assignment = models.ForeignKey('catalog.Assignment', on_delete=models.SET_NULL, null=True)
 	submitted_at = models.DateField(auto_now_add=True, blank=True)
-	files = models.FileField(upload_to='', help_text='Submit', null=True)
+	student =  models.ForeignKey("auth.User",
+                                 limit_choices_to={'groups__name': "Student"},
+                                 on_delete=models.SET_NULL, null=True)
+	files = models.FileField(upload_to=get_files_path,
+							 null=True,
+							 max_length=255)
 
 	def __str__(self):
-		return "temp for now"
+		str_ = str(self.student) + " submitted to " + str(self.files)
+		return str_ + " at " + str(self.submitted_at)
--- autograder/submission_grader/views.py
+++ autograder/submission_grader/views.py
@@ -10,22 +10,21 @@ Last Edited by:	Eric Zair
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.models import User
 from accounts.views import not_instructor_throw_error
-from django.http import HttpResponseForbidden
 from . import forms
+from . import models
 
 
 @login_required
+# View page for when a student submits and assignment.
 def submit_view(request):
-	if request.method == 'POST':
-		form = forms.SubmissionUploadForm(request.POST or request.FILES)
-		# TEST STATEMENT
-		if form.is_valid():
+	form = forms.SubmissionUploadForm(request.POST or None, request.FILES or None)
+	
+	if request.method == 'POST'and form.is_valid():
+			models.Submission(assignment=form.cleaned_data['assignment'],
+							  student=request.user,
+							  files=form.cleaned_data['files'])
 			form.save()
-			return render(request, 'submission_grader/submit.html', {'form': form})
-	else:
-		form = forms.SubmissionUploadForm()
-		if form.is_valid():
-			form.save()
-			return render(request, 'submission_grader/submit.html', {'form': form})
+			print("I got here")
+			return HttpResponseRedirect('/catalog/')
+	return render(request, 'submission_grader/submit.html', {'form': form})
