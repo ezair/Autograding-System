a7cb86b5989eef463946299fe97d46ace7b514af
EZ <zaireric@gmail.com>
2019-04-30 20:16:32 -0400

    I had to change the catalog.Assignment model, because is just works out much nicer. I gived a template issue. The submit assignment view is now done. Linked it to the submission button for the moment. I will next proceed with the submission detail view.

--- autograder/submission_grader/views.py
+++ autograder/submission_grader/views.py
@@ -7,8 +7,8 @@ Description:	Contains all views/pages used for the submission_grader app
 Last Edited on: 04/25/2019
 Last Edited by:	Eric Zair
 '''
-from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, Http404
 from django.contrib.auth.decorators import login_required
 from accounts.views import not_instructor_throw_error
 from . import forms
@@ -19,8 +19,14 @@ from catalog.models import Assignment, Project
 @login_required
 # View page for when a student submits an assignment.
 def submit_view(request, pk):
-	# If the assignment does not exist, we have an issue, m8.
-	assignment = get_object_or_404(Assignment, project=pk)
+	# We need to account for if the assignment even exists in the first place.
+	if not Assignment.objects.filter(project=pk, assigned_student=request.user).exists():
+		raise Http404
+
+	# Aye, so the assignment does exist, so we take the firsts one in existance, m8.
+	# We filter it for the first assignment, just incase there is another model of the project.
+	# There should NOT EVER BE, but stuff happens.
+	assignment = Assignment.objects.filter(project=pk).order_by('id').first()
 	if request.method == 'POST':
 		form = forms.SubmissionUploadForm(student=request.user,
 										  assignment=assignment,
