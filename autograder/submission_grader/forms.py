7d9e938a7a6be30ef740ed6ea892f38430453568
EZ <zaireric@gmail.com>
2019-05-03 00:11:42 -0400

    After making the form, so now it will unzip if given a zipfuile

--- autograder/submission_grader/forms.py
+++ autograder/submission_grader/forms.py
@@ -10,7 +10,7 @@ Last edited on:	04/25/2019
 from django.contrib.auth.models import User
 from django import forms
 from .models import *
-
+import zipfile
 
 # This will be the form for when a student uploads
 # the code they wrote for an assignment.
@@ -28,6 +28,7 @@ class SubmissionUploadForm(forms.ModelForm):
 		self.submitted = kwargs.pop('submitted_at')
 		super(SubmissionUploadForm, self).__init__(*args, **kwargs)
 
+
 	# Override the form, so that it gets the args that it needs.
 	def save(self, commit=True):
 		submission = super(SubmissionUploadForm, self).save(commit=False)
@@ -35,4 +36,9 @@ class SubmissionUploadForm(forms.ModelForm):
 		submission.student = self.student
 		if commit:
 			submission.save()
+			# If the file is a zip file, we want to extract what is in it immediately.
+			if submission.files.path.endswith('.zip'):
+				zip_file = zipfile.ZipFile(submission.files.path, 'r')
+				zip_file.extractall(submission.files.path.split(submission.file_name())[0])
+				zip_file.close()
 		return submission
