da15232312555f3126bf1fff2dcfd6ee5e460456
EZ <zaireric@gmail.com>
2019-05-02 22:37:57 -0400

    commented template tags file

--- autograder/accounts/templatetags/accounts_template_tags.py
+++ autograder/accounts/templatetags/accounts_template_tags.py
@@ -16,10 +16,12 @@ Last edited on:	04/22/2019
 from django import template
 from submission_grader.models import Submission
 
+
 # Instance created so that we can create custom templates tags.
 # Template tags in this app will have respect to user's and permissons.
 # An example of this being checking to see what group a user is in.
 register = template.Library()
+
 
 # This method is used so that in our html template tags
 # we can check to see what group a user is in.
@@ -28,6 +30,9 @@ register = template.Library()
 def has_group(user, group_name):
 	return user.groups.filter(name=group_name).exists()
 
+
+# Given an assignment, we can find the last created submission associated
+# with it.
 @register.filter(name='get_recent_submission')
 def get_recent_submission(assignment):
 	if Submission.objects.filter(assignment=assignment.pk, student=assignment.assigned_student.pk).exists():
@@ -36,6 +41,9 @@ def get_recent_submission(assignment):
 		return submission.file_name()
 	return "No submissions"
 
+
+# Given an assignment, we can find the last crated submissions's pk
+# associated with it.
 @register.filter(name='get_recent_submission_pk')
 def get_recent_submission_pk(assignment):
 	if Submission.objects.filter(assignment=assignment.pk,
