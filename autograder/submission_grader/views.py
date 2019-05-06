2e2e1452fa02fe198bee44c76d93bf9521435eea
EZ <zaireric@gmail.com>
2019-05-05 20:37:16 -0400

    Addeed decor hanlder so that you must be logged in to view any of the pages.

--- autograder/submission_grader/views.py
+++ autograder/submission_grader/views.py
@@ -4,20 +4,20 @@ File:	submission_grader/views.py
 Description:	Contains all views/pages used for the submission_grader app
 				These views will include things like students submitting
 				things to git and such.
-Last Edited on: 05/1/2019
+Last Edited on: 05/05/2019
 Last Edited by:	Eric Zair
 '''
-from django.views import generic
-from django.shortcuts import render
-from django.http import HttpResponseRedirect, Http404
+from django.views.generic import DeleteView, DetailView, ListView
 from django.contrib.auth.decorators import login_required
 from accounts.views import not_instructor_throw_error
+from django.http import HttpResponseRedirect, Http404
+from django.utils.decorators import method_decorator
+from catalog.models import Assignment, Project
+from django.urls import reverse_lazy, reverse
+from django.shortcuts import render
+from datetime import datetime
 from . import forms
 from . import models
-from catalog.models import Assignment, Project
-from datetime import datetime
-from django.views.generic import DeleteView, DetailView
-from django.urls import reverse_lazy, reverse
 
 
 @login_required
@@ -48,6 +48,9 @@ def submit_view(request, pk):
 	return render(request, 'submission_grader/submit.html', {'form': form})
 
 
+# This view brings the user to a page that prompts them to
+# confirm that they want to delete a submission.
+# They must click "confirm" for the submission to be deleted
 class DeleteSubmissionView(DeleteView):
 	model = models.Submission
 	template = 'submission_confirm_delete'
@@ -58,7 +61,14 @@ class DeleteSubmissionView(DeleteView):
 		return reverse_lazy('catalog-assignment_detail',
 	 						kwargs={'pk': assignment})
 
+	# User must be logged in to see this page.
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(DeleteSubmissionView, self).dispatch(*args, **kwargs)
 
+
+# NOT SURE IF THIS IS NEEDED OR NOT.
+# MORE ON THIS LATER.
 class SubmissionDetailView(DetailView):
 	model = models.Submission
 	template = 'submission_grader/submission_detail.html'
@@ -68,6 +78,19 @@ class SubmissionDetailView(DetailView):
 		return reverse_lazy('submission_grader-submission_detail',
 							 kwargs={'pk': self.object.pk})
 
-class SubmissionListView(generic.ListView):
+	# User must be logged in to see this page.
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(SubmissionDetailView, self).dispatch(*args, **kwargs)
+
+
+# NOTE SURE IF THIS IS NEEDED OR NOT.
+# MORE ON THIS LATER.
+class SubmissionListView(ListView):
 	model = models.Submission
 	template = 'submission_grader/submission_list.html'
+
+	# User must be logged in to see this page.
+	@method_decorator(login_required)
+	def dispatch(self, *args, **kwargs):
+		return super(SubmissionListView, self).dispatch(*args, **kwargs)
